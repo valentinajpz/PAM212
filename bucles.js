2e1b1c7643964df48080e42bcd6109089ec4b369
valentinajpz <124048965@upq.edu.mx>
2025-09-11 11:20:18 -0600

    Suma y concatenación con funciones

--- bucles.js
+++ bucles.js
@@ -1,6 +1,8 @@
-function greeting(personName) {
-    console.log('Hello' + personName);
+function add(n1, n2) {
+    console.log(n1 + n2);
 }
- greeting('Kara');
- greeting('Marcus');
- greeting('Konor');
+
+add(3, 2);
+add(100, 200);
+add(33245, 24345);
+add(3, 'sjghdghz');
