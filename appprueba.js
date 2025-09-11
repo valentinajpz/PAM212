149350313c70b07843ddd4c6ecc8b631765e8e6e
valentinajpz <124048965@upq.edu.mx>
2025-09-11 10:34:14 -0600

    Comparación de variables con operadores lógicos

--- appprueba.js
+++ appprueba.js
@@ -1,6 +1,9 @@
 let name = "John";
 let lastName = "Chavez";
+let numberOne = 100;
+let numberTwo = 500;
 
 let completeName = name + lastName;
+let result = numberOne > numberTwo;
 
-console.log(completeName);
+console.log(result);
