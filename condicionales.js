28ddc0a37716fcdc8805ddea83167bd4ce53b2ba
valentinajpz <124048965@upq.edu.mx>
2025-09-11 10:54:01 -0600

    Comparar dentro de una sola variable multiples casos

--- condicionales.js
+++ condicionales.js
@@ -1,21 +1,10 @@
-let passwordDB = "pepe123"
-let score = 70;
+let typeCard = "Debitd Card";
 
-let input = "jhbfhhsdfj"
-
-let result = input ==passwordDB;
-if (score > 30) {
-    console.log("You need to practice more");
-}
-
-if (result === true) {
-    console.log("Login Correcto");
-else if (score > 15 ) {
-    console.log("Estas mejorando");
-}
-
-if (result === false) {
-    console.log("Login Incorrecto");
-else {
-    console.log("You need to follos this tutorial");
+switch(typeCard) {
+    case "Debid Card":
+        console.log("This is a debid card");
+        break;
+    case "credit Card" :
+        console.log("This is a credit card");
+        break;
 }
