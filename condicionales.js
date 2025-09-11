e5d277681a2d221bad02a2640a7d880269841dd2
valentinajpz <124048965@upq.edu.mx>
2025-09-11 10:41:15 -0600

    Utilizar condicionales para un Login

---
+++ condicionales.js
@@ -0,0 +1,13 @@
+let passwordDB = "pepe123"
+
+let input = "jhbfhhsdfj"
+
+let result = input ==passwordDB;
+
+if (result === true) {
+    console.log("Login Correcto");
+}
+
+if (result === false) {
+    console.log("Login Incorrecto");
+}
