613000c8796e2f9e4f43a4d71baed27ec13b4081
valentinajpz <124048965@upq.edu.mx>
2025-09-11 10:49:23 -0600

    Utilizar varias condicionales para comparar

--- condicionales.js
+++ condicionales.js
@@ -1,13 +1,21 @@
 let passwordDB = "pepe123"
+let score = 70;
 
 let input = "jhbfhhsdfj"
 
 let result = input ==passwordDB;
+if (score > 30) {
+    console.log("You need to practice more");
+}
 
 if (result === true) {
     console.log("Login Correcto");
+else if (score > 15 ) {
+    console.log("Estas mejorando");
 }
 
 if (result === false) {
     console.log("Login Incorrecto");
+else {
+    console.log("You need to follos this tutorial");
 }
